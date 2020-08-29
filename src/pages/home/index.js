15f5f57340c23f44c4cba52686a5a19bc0c78a9d
Jose Henrique <vasco0007@gmail.com>
2020-08-29 14:17:07 -0300

    Criado componente Header e inserido na home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import Container from '@material-ui/core/Container'
+
 import AppBar from '../../components/appBar'
+import Header from '../../components/header'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <AppBar />
-    </div>
+      <Container>
+        <Header text="Teste" />
+        <div>Conteudo</div>
+      </Container>
+    </>
   )
 }
